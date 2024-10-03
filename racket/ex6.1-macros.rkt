dc147ce6680f727b7167d49b6fc52f3a8f607c28
Andrea Franchini <hello@andreafranchini.com>
2024-10-03 13:17:17 +0200

    add more examples on macros

---
+++ racket/ex6.1-macros.rkt
@@ -0,0 +1,98 @@
+#lang racket
+
+;; MACROS
+
+; For example: (and (expr a) ... ) is a macro!
+
+(and (even? 4) (string? 4)) ; => #f
+(and #t (even? 2) (number? 2)) ; => #t
+
+; BUT we can implement and as a function!
+(define (my-and x . xs)
+  (display x) (display " ") (displayln xs)
+  (if (null? xs)
+      x
+      (when (first xs)
+        (apply my-and xs))))
+
+(if (equal? (my-and #t (even? 4) (number? 4)) #t)
+    (displayln "PASS")
+    (displayln "FAIL"))
+
+; Ideally, as a macro, we when we have:
+;
+; (and (expr a) (expr b) (expr c))
+;
+; we would rewrite it like this:
+;
+; (if (expr a)
+;    (if (expr b)
+;        (expr c)
+;        #f)
+;    #f)
+
+(newline)(newline)
+
+; IS THERE ANYTHING WE CAN'T IMPLEMENT AS A FUNCTION,
+; BUT REQUIRES A MACRO?
+
+; let's say that we want to make a 'report' function
+; that receives an expression, prints it, and returns
+; its output
+
+; Can a function do that?
+
+(define (report-f lst)
+  (displayln lst)
+  lst)
+
+(report-f (* 1 2 3 4))
+
+; We need to use a macro!
+
+(define-syntax report-m
+  (syntax-rules ()
+    [(_ EXPR)
+     (begin
+       (display "Report: ")(displayln (quote EXPR))
+       EXPR)]))
+
+(report-m (* 1 2 3 4))
+
+
+(newline)(newline)
+
+; LET'S GET BACK TO THE 'AND' MACRO
+
+(define-syntax and-m
+  (syntax-rules ()
+    [(_) #t]
+    [(_ COND) COND]
+    [(_ COND OTHER-CONDS ...)
+     (if COND
+         (and-m OTHER-CONDS ...)
+         #f)]))
+
+(and-m)
+(and-m 42)
+(and-m 42 #t #f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
